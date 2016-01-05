d3567474ccfe4f7009321298d17f6f511d4e1cb6
ursidal <sylvain.vinet@free.fr>
2016-01-05 23:16:46 +0100

    modifications on index.html and main.js for testing Vue

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,24 +1,29 @@
 //markdown compiler to pimp out (href->v-link)
-var marked = require('marked');
+//var marked = require('marked');
 
 //My Model: Data example
 
-var data = {
-  txt: "La terre est [bleue] [comme une orange]"
-  links: {
-    bleue: {
-      txt: "couleur primaire"
-    }
-    comme_une_orange: {
-      txt: "le fruit rond"
-    }
-  }
-} 
-
+var poeme = {
+  txt: "La terre est [bleue] [comme une orange]",
+  //links: {
+    // bleue: {
+    //   txt: "couleur primaire"
+    // },
+    // comme_une_orange: {
+    //   txt: "le fruit rond"
+    // }
+  //}
+}
 
 // create a Vue instance, or, a "ViewModel"
 // which links the View and the Model
 var notebook = new Vue({
-  el: '#notebook',
-  data: data
+  el: '#elt',
+  data: {
+    txt: "La *terre* est [bleue] [comme une orange]"
+  },
+  methods: {
+    marked: marked
+  }
 })
+console.log(notebook.marked(poeme.txt))
