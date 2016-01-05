105cd7903363dd61ab7f5335a88d5365de64eae4
ursidal <sylvain.vinet@gmail.com>
2016-01-05 11:51:57 +0100

    Update main.js

--- ressources/js/main.js
+++ ressources/js/main.js
@@ -1,11 +1,21 @@
-// this is our Model
-var exampleData = {
-  name: 'Vue.js'
-}
+//My Model: Data example
+
+var data = {
+  txt: "La terre est [bleue] [comme une orange]"
+  links: {
+    bleue: {
+      txt: "couleur primaire"
+    }
+    comme_une_orange: {
+      txt: "le fruit rond"
+    }
+  }
+} 
+
 
 // create a Vue instance, or, a "ViewModel"
 // which links the View and the Model
-var exampleVM = new Vue({
-  el: '#example-1',
-  data: exampleData
+var notebook = new Vue({
+  el: '#notebook',
+  data: data
 })
