5c752f45409de30b0d0b7a29f16c2d07b2cce986
ursidal <sylvain.vinet@free.fr>
2016-01-31 22:52:20 +0100

    ajout de pouchdb

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,43 @@
 //markdown compiler to pimp out (href->v-link)
 //var marked = require('marked');
 
+var configdb = new PouchDB('treefold-config');
+
+function initDb(){}
+  configdb.get('config').catch(function (err) {
+  if (err.status === 404) { // not found!
+    return {
+      _id: 'config',
+      namedb: 'treefold-notebook',
+      urldb: ''
+    };
+  } else { // hm, some other error
+    throw err;
+  }
+}).then(function (configDoc) {
+  var db = new PouchDB(namedb);
+  if(urldb!=''){
+    var remotedb = new Pouchdb(urldb+"/"+namedb);
+    db.sync(remoteDb, {live: true});
+  }
+  else {
+    var remotedb = false;
+  }
+  db.changes({
+    since: 'now',
+    live: true
+  }).on('change', updateList);
+}).catch(function (err) {
+  console.log("Voici l'erreur lors de l'initialisation: "+err);
+});
+
+
+
+
+
 //My Model: Data example
+
+
 
 var poeme = {
   txt: "La terre est [bleue] [comme une orange]",
